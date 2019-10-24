85670eedc4fe47bdcbd3d7b5510e4f775a7df580
kasmenai <kasmenai@gmail.com>
2019-10-24 13:10:04 +0300

    add basic hapi server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const Hapi = require('@hapi/hapi');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+
+        return 'Hello World!';
+    }
+});
+
+exports.init = async () => {
+    await server.initialize();
+    return server;
+};
+
+exports.start = async () => {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+    return server;
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
