d065aecd23517a9185f2ff09433f36b43e70fb7b
kasmenai <kasmenai@gmail.com>
2019-11-13 11:45:20 +0300

    add all reducers

--- src/client/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import authorsReducer from './authors';
-
-export default combineReducers({ authorsData: authorsReducer });
--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,6 +1,14 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
+import authorsData from '../pages/authors/reducers';
+import websitesData from '../pages/websites/reducers';
+import articlesData from '../pages/articles/reducers';
+
+const rootReducer = combineReducers({
+  authorsData,
+  websitesData,
+  articlesData,
+});
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
