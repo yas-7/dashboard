5faae2d9306bcc15d364c423de7061ddaef53b99
kasmenai <kasmenai@gmail.com>
2019-11-14 10:21:40 +0300

    serve html content from the dist directory

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,12 @@
 const Hapi = require('@hapi/hapi');
+const Path = require('path');
+const Inert = require('@hapi/inert');
 const { configureRoutes } = require('./routes');
 
 const server = Hapi.server({
   port: process.env.PORT || 8080,
   host: process.env.HOST || 'localhost',
+  routes: { files: { relativeTo: Path.join(__dirname, '../../dist') } },
 });
 
 configureRoutes(server);
@@ -14,6 +17,30 @@ exports.init = async () => {
 };
 
 exports.start = async () => {
+  await server.register(Inert);
+  server.route([
+    {
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+          index: true,
+        },
+      },
+    },
+    {
+      method: 'GET',
+      path: '/authors',
+      handler: (_, h) => h.file('index.html'),
+    },
+    {
+      method: 'GET',
+      path: '/websites',
+      handler: (_, h) => h.file('index.html'),
+    },
+  ]);
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
   return server;
