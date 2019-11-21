c7808ad220357318fd4cd8c1de02a8304bdf3331
kasmenai <kasmenai@gmail.com>
2019-11-21 16:41:27 +0300

    refactor article page

--- src/client/pages/articles/containers/ArticlesPage.jsx
+++ src/client/pages/articles/containers/ArticlesPage.jsx
@@ -1,16 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import ArticlesContainer from './ArticlesContainer';
+import ArticlesList from '../components/ArticlesList';
 import EditContainer from './EditContainer';
+import Popup from '../../../components/Popup';
+import Pagination from '../../../components/Pagination';
+import Header from '../../../components/Header';
+import { editArticleFields, fetchArticles, changePagination } from '../actions';
+import SearchContainer from './SearchContainer';
 
-const ArticlePage = ({ isPopupActive }) => (
-  <div>
-    <ArticlesContainer />
-    {isPopupActive ? <EditContainer /> : null}
-  </div>
-);
+const ArticlesPage = (props) => {
+  const {
+    isPopupActive,
+    editArticleFields,
+    changePagination,
+    fetchArticles,
+    pagination,
+    order,
+    filter,
+  } = props;
 
-ArticlePage.propTypes = { isPopupActive: PropTypes.bool };
+  const paginationProps = {
+    pagination,
+    order,
+    changePagination,
+    fetchData: fetchArticles,
+    filter,
+  };
 
-export default connect((state) => ({ isPopupActive: state.articlesData.isPopupActive }))(ArticlePage);
+  return (
+    <React.Fragment>
+      <Header addHandler={editArticleFields}>
+        <SearchContainer />
+      </Header>
+      <ArticlesList />
+      <Pagination paginationProps={paginationProps} />
+      <Popup isPopupActive={isPopupActive}><EditContainer /></Popup>
+    </React.Fragment>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  isPopupActive: state.articlesData.isPopupActive,
+  order: state.articlesData.order,
+  filter: state.articlesData.filter,
+  pagination: state.articlesData.pagination,
+});
+
+const mapDispatchToProps = {
+  editArticleFields,
+  changePagination,
+  fetchArticles,
+};
+
+
+ArticlesPage.propTypes = {
+  isPopupActive: PropTypes.bool,
+  editArticleFields: PropTypes.func.isRequired,
+  changePagination: PropTypes.func.isRequired,
+  fetchArticles: PropTypes.func.isRequired,
+  pagination: PropTypes.object.isRequired,
+  order: PropTypes.object.isRequired,
+  filter: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArticlesPage);
