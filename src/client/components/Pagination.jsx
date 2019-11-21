3a7e676ca9ac4183f963ef33ff76976b464c03dd
kasmenai <kasmenai@gmail.com>
2019-11-21 21:59:22 +0300

    add items per page

--- src/client/components/Pagination.jsx
+++ src/client/components/Pagination.jsx
@@ -10,6 +10,7 @@ const Pagination = ({ paginationProps }) => {
     filter,
   } = paginationProps;
 
+  const itemsPerPage = [ 2, 5, 10 ];
   const { offset, limit, count } = pagination;
   const totalPage = Math.ceil(count / limit);
   const currentPage = 1 + offset / limit;
@@ -20,38 +21,61 @@ const Pagination = ({ paginationProps }) => {
     fetchData({ ...filter, ...order, offset: newOffset, limit });
   };
 
+  const handleItemsPerPage = ({ target }) => {
+    const { value } = target;
+    changePagination({ limit: Number(value) });
+    fetchData({ ...filter, ...order, offset: 0, limit: Number(value) });
+  };
+
   return (
     <div className="pagination">
-      <span className="pagination__info">{currentPage}</span>
-      <span className="pagination__info">of</span>
-      <span className="pagination__info">{totalPage}</span>
-      <button
-        className="btn pagination__control"
-        type="button"
-        disabled={offset <= 0}
-        onClick={() => handleClick(-1)}
-      >
+      <div className="pagination__group">
+        <span className="pagination__info">{currentPage}</span>
+        <span className="pagination__info">of</span>
+        <span className="pagination__info">{totalPage}</span>
+        <button
+          className="btn pagination__control"
+          type="button"
+          disabled={offset <= 0}
+          onClick={() => handleClick(-1)}
+        >
         Prev
-      </button>
-      <button
-        className="btn pagination__control"
-        type="button"
-        disabled={offset + limit >= count}
-        onClick={() => handleClick()}
-      >
+        </button>
+        <button
+          className="btn pagination__control"
+          type="button"
+          disabled={(offset + limit) >= count}
+          onClick={() => handleClick()}
+        >
         Next
-      </button>
+        </button>
+      </div>
+      <div className="pagination__group">
+        <span className="pagination__info">Items per page</span>
+        <select
+          className="pagination__select"
+          value={limit}
+          onChange={handleItemsPerPage}
+          name="AuthorId"
+        >
+          {itemsPerPage.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
 
 Pagination.propTypes = {
   paginationProps: PropTypes.object.isRequired,
-  fetchData: PropTypes.func.isRequired,
-  changePagination: PropTypes.func.isRequired,
-  pagination: PropTypes.object.isRequired,
-  order: PropTypes.object.isRequired,
-  filter: PropTypes.object.isRequired,
+  fetchData: PropTypes.func,
+  changePagination: PropTypes.func,
+  pagination: PropTypes.object,
+  order: PropTypes.object,
+  filter: PropTypes.object,
 };
 
 export default Pagination;
